e46fe56f1ba6ff46b5df6798fb95402b55c2ae60
Amreth73 <117499632+Amreth73@users.noreply.github.com>
2023-12-28 17:27:06 +0530

    Delete class directory
    deleted

--- class/Components/DynamicRender.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function DynamicRender({isLoggedIn}) {
-  return (
-    <div>
-        {isLoggedIn ? <p>Welcome, user!</p>:<p>Please log in.</p>}
-    </div>
-  )
-}
--- class/day2/daytwofirst.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Daytwofirst() {
-  return (
-    <div>
-        <h1>HELLO, WORLD!</h1>
-    </div>
-  )
-}
